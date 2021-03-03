a5adec7ebd41e278be50d455a34dd746c6d8a14f
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-03 15:31:33 -0600

    added field.js

---
+++ src/scripts/field.js
@@ -0,0 +1,16 @@
+let growing = {};
+
+const addPlant = (seedObj) => {
+    return growing.push(seedObj);
+};
+
+const usePlants = (growing) => {
+    return growing;
+};
+
+// Array.isArray(seedObj)
+
+
+// let seedObj = createCorn;
+
+// console.log(seedObj);
