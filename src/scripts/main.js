4d19f0de39c04e6d43bb7d38e9bf6b1657c8ddda
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-02 18:40:02 -0600

    created seeds

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,13 @@ import { createPlan } from "./plan.js"
 const yearlyPlan = createPlan();
 
 console.log(yearlyPlan);
+
+import { createAsparagus } from "./seeds/asparagus.js"
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
+
+import { createCorn } from "./seeds/corn.js"
+
+const cornSeed = createCorn()
+console.log(cornSeed)
